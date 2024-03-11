84d9dbae5469656d6820e914c0c457c94c94dd6e
Manjularaj <manjularajanap@gmail.com>
2024-03-10 21:37:30 -0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA1_Fields_And_Properties_ManjulaRajan
+{
+    public class Person
+    {
+       
+             
+
+            private string _name;
+
+            //Property
+            public string Name
+            {
+                get { return _name; }
+                set 
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        _name = value;
+
+                    else
+                        throw new ArgumentException("Name cannot be null or empty");
+                            
+                }
+            }
+
+            //Read only property; only includes a getter
+            public string ReadOnlyName
+            {
+                get { return _name; }
+            }
+
+            //Write only property; only includes a setter
+            public string WriteOnlyName
+            {
+                set { _name = value; }
+            }
+        
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GA1_Fields_And_Properties_ManjulaRajan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.Name = "John Doe";// Testing setter with validation
+            Console.WriteLine(person.Name); // Testing getter
+
+            
+            Console.WriteLine(person.ReadOnlyName); //Testing read only property
+
+            //person.WriteOnlyName = "Jane Doe";
+
+
+        }//End Main
+
+
+    }//class
+
+
+}//namespace
